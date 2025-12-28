8643a62244eb304b3f1dcf8bb2006a7e58651c91
Pascal <pascal.letsch2002@gmail.com>
2025-12-28 14:02:25 +0100

    Remove unused `Connection` class, comment out `EchoApplication` server, and add PostgreSQL setup with Docker Compose and IntelliJ datasource configuration.

--- src/main/java/at/technikum/Main.java
+++ src/main/java/at/technikum/Main.java
@@ -1,6 +1,5 @@
 package at.technikum;
 
-import at.technikum.application.echo.EchoApplication;
 import at.technikum.server.Server;
 import at.technikum.application.mrp.MrpApplication;
 
@@ -8,8 +7,9 @@ public class Main {
     public static void main(String[] args) {
 
         Server server = new Server(8080, new MrpApplication());
-        Server echoServer = new Server(3333, new EchoApplication());
+        //Server echoServer = new Server(3333, new EchoApplication());
         server.start();
-        echoServer.start();
+        //echoServer.start();
+
     }
 }
